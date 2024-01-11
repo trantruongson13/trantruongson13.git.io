56ae118efef12aea25b5234880bed29a3c96ad50
trantruongson13 <stran1301@gmail.com>
2024-01-12 00:48:01 +0700

    add file html css js

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = "SƠN";
+const giftUrl = "https://www.facebook.com/truongson.tran.108/";
+const eventName = "Chúc Mừng Sinh Nhật";
+const titleCard = " HAPPY BIRTH TO ME";
+const contentCard =
+  "Hôm nay là một ngày đặc biệt, đánh dấu 1 năm trưởng thành hơn sau những nỗ lực, cố gắng rất nhiều. Chúc cho tôi bước sang tuổi mới thành công hơn, đạt được những thứ mình đang cố gắng để có được và luôn vui vẻ.";
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ:
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = "son.jpg";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
